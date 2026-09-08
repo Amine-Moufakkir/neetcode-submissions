9486133cc833d3765faf6eee41583a560f07919c
Amine Moufakkir <125572683+Amine-Moufakkir@users.noreply.github.com>
2026-09-08 14:33:22 +0100

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int trap(int[] height) {
+        // [0, 2, 0, 3, 1, 0, 1, 3, 2, 1]
+        // 
+        //
+        int[] prefix = new int[height.length];
+        int[] suffix = new int[height.length];
+        int l = 0, r = 0;
+        for (int j = 0; j < height.length; j++) {
+            if (height[j] > l) {
+                l = height[j];
+            }
+            if (height[height.length - 1 - j] > r) {
+                r = height[height.length - 1 - j];
+            }
+            prefix[j] = l;
+            suffix[height.length - 1 - j] = r;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < height.length; i++) {
+            int trappedWater = Math.min(prefix[i], suffix[i]) - height[i];
+            sum += trappedWater > 0 ? trappedWater : 0;
+        }
+        return sum;
+    }
+}
